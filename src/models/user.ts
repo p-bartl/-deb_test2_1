bb55962e18f3e0f25537d1b0d5ecfe4e8f0917ae
P. Bartl <pascal.bartl@bmw.de>
2021-11-09 18:26:20 +0100

    initial commit15

--- src/models/user.ts
+++ src/models/user.ts
@@ -13,22 +13,25 @@ export type User = {
 export class UserStore {
     async create(u: User): Promise<User> {
         try {
-            const conn = await Client.connect()
-            const sql = 'INSERT INTO users (first_name, last_name, username'
-            const hash = bcrypt.hashSync(
-                u.password + pepper,
-                parseInt(saltRounds)
-            );
-            const result = await conn.query(sql, [u.username, hash])
-            const user = result.rows[0]
-            
-            conn.release()
-            return user
+          // @ts-ignore
+          const conn = await Client.connect()
+          const sql = 'INSERT INTO users (username, password_digest) VALUES($1, $2) RETURNING *'
+    
+          const hash = bcrypt.hashSync(
+            u.password + pepper, 
+            parseInt(saltRounds)
+          );
+    
+          const result = await conn.query(sql, [u.username, hash])
+          const user = result.rows[0]
+    
+          conn.release()
+    
+          return user
         } catch(err) {
-            throw new Error(`unable to create user (${u.username}): ${err}`)
-        }
-
-    }
+          throw new Error(`unable create user (${u.username}): ${err}`)
+        } 
+      }
 
     async authenticate(username: string, password: string): Promise<User | null> {
         const conn = await Client.connect()
